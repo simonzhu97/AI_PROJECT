3818e4c8bef11afefe39f39635fb157bebbe7fff
MintForever <lin_xinyue@outlook.com>
2020-10-25 17:27:16 -0400

    modified py and added result.txt

--- readjson.py
+++ readjson.py
@@ -1,9 +1,25 @@
+
 import json
+
+file = open('result.txt','w')
+
+
 tweets=[]
 result = []
+count = 0
 for line in open('file2.jsonl-00000-of-00192','r'):
+    count +=1
     data = json.loads(line)
-    for i in data['input_sequence']:
-         result.append(i)
+    result.append(data['input_sequence'])
+    file.write(str(data['input_sequence'][0]))
+    file.write('\n')
+    # for i in data['input_sequence']:
+    #      result.append(i)
 
-print(result[0])
+# print(result[0])
+# print("----------------------------------------------------------------")
+# print(result[1])
+# print(len(result))
+# print(count)
+file.close()
+
