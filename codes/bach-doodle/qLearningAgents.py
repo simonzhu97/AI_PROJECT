955fc2fb184369e7f9060cd112cdc439f28a0cb7
Mint Lin <lin_xinyue@outlook.com>
2020-11-02 23:54:18 -0500

    added reward calculation

--- codes/bach-doodle/qLearningAgents.py
+++ codes/bach-doodle/qLearningAgents.py
@@ -51,3 +51,62 @@ class layout:
         '''{(start_time, end_time):(origin_note, [new_notes])}'''
         pass
 
+
+
+
+def get_major_notes(root):
+    major_third = root + 4
+    perfect_fifth = root + 7
+    second_inversion = [root-8, root-5]
+    consonance = [major_third, perfect_fifth, second_inversion[0], second_inversion[1]]
+    return consonance
+
+def get_dissonance(root):
+    return [root+1, root+2, root-1, root-2, root-11, root-10, root+11, root+10]
+
+#take in two pitch values; root is the original note and pitch is note being evaluated
+# using major chords only
+def get_major_reward(root, pitch):
+    highest_reward = get_major_notes(root)
+    dissonance = get_dissonance(root)
+    octaves = [root+12, root-12]
+    if pitch in highest_reward:
+        return 50
+    elif pitch in dissonance or pitch == root:
+        return -50
+    elif pitch in octaves:
+        return 10
+    elif abs(pitch-root) < 12:
+        return 20
+    # span more than one octave
+    else:
+        return -30
+
+
+def get_comparison_reward(root, pitch, prev_root, prev_pitch):
+    reward = 0
+    pitch_dissonance = get_dissonance(pitch)
+    if prev_root in pitch_dissonance or prev_pitch in pitch_dissonance:
+        reward -= 40
+    if abs(pitch-prev_root) > 12 or abs(pitch-prev_pitch) > 12:
+        reward -= 30
+    if root == prev_root and pitch == prev_pitch:
+        reward -= 10
+    consonance = get_major_notes(root)
+    consonance.extend(root+12, root-12)
+    sub = [ele for ele in consonance if ele in [root, pitch, prev_root, prev_root]]
+    if len(sub) == 4:
+        reward += 40
+    if len(sub) == 3:
+        reward += 30
+    if (root < max(prev_root, prev_pitch) and root > min(prev_root, prev_pitch)) or (pitch < max(prev_root, prev_pitch) and pitch > min(prev_root, prev_pitch)):
+        reward += 10
+
+    return reward
+        
+
+def get_total_reward(root, pitch, prev_root, prev_pitch):
+    return get_major_reward(root, pitch)+get_comparison_reward(root, pitch, prev_root, prev_pitch)
+    
+
+
