8847f4128bc32e134390118a744b66e0aed4a5a8
kevinsunofficial <kevin.sun0612@hotmail.com>
2020-11-06 16:36:00 -0500

    allowed read in file

--- codes/bach-doodle/parse_midi.py
+++ codes/bach-doodle/parse_midi.py
@@ -5,9 +5,12 @@ from note_seq.protobuf import music_pb2 as mpb
 import os
 import os.path as osp
 
-input_dir = '/u/ys4aj/YuchenSun/Course/CS4710/AI_PROJECT/codes/bach-doodle/infile/'
+input_dir = '/u/ys4aj/YuchenSun/Course/CS4710/AI_PROJECT/codes/bach-doodle/magenta_midi/'
 infile = osp.join(input_dir,'2020-10-28_224250_1.midi')
+output_dir = '/u/ys4aj/YuchenSun/Course/CS4710/AI_PROJECT/codes/bach-doodle/magenta_txt/'
+outfile = osp.join(output_dir,'2020-10-28_224250_1.txt')
 
+print('Parsing midi files...')
 seq = note_seq.midi_file_to_note_sequence(infile)
 origin = [i for i in seq.notes if i.end_time <= 8.0]
 new = [i for i in seq.notes if i.start_time >= 8.0]
@@ -18,9 +21,21 @@ def equal(note1,note2):
     dur1, dur2 = note1.end_time-note1.start_time, note2.end_time-note2.start_time
     return dur1 == dur2 and note1.pitch == note2.pitch
 
+print('Cleansing new...')
 for o in origin:
     for c in new:
         if equal(o,c):
             new.remove(c)
 
-layout_info = [origin, new]
+print('Writing output file...')
+with open(outfile,'w') as file:
+    for n in origin:
+        start, end, pitch = n.start_time, n.end_time, n.pitch
+        line = ','.join([str(start),str(end),str(pitch)+'\n'])
+        file.write(line)
+    file.write('------------------------\n')
+    for n in new:
+        start, end, pitch = n.start_time, n.end_time, n.pitch
+        line = ','.join([str(start),str(end),str(pitch)+'\n'])
+        file.write(line)
+print('FINISHED')
