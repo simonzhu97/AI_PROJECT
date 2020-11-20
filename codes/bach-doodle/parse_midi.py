2c660039a4489b5a0eaa7e8346fd89e78b28b7a1
kevinsunofficial <kevin.sun0612@hotmail.com>
2020-11-20 18:25:09 -0500

    lamer

--- codes/bach-doodle/parse_midi.py
+++ codes/bach-doodle/parse_midi.py
@@ -10,47 +10,38 @@ input_dir = '/u/ys4aj/YuchenSun/Course/CS4710/AI_PROJECT/codes/bach-doodle/magen
 output_dir = '/u/ys4aj/YuchenSun/Course/CS4710/AI_PROJECT/codes/bach-doodle/magenta_txt/'
 # outfile = osp.join(output_dir,'2020-10-28_224250_1.txt')
 
-for json_dir in os.listdir(input_dir):
-    full_json_dir = osp.join(input_dir,json_dir)
-    for item in os.listdir(full_json_dir):
-        midi_dir = osp.join(full_json_dir,item)
-        
-
-
-
-
-
-
-
-
-
-
-print('Parsing midi files...')
-seq = note_seq.midi_file_to_note_sequence(infile)
-origin = [i for i in seq.notes if i.end_time <= 8.0]
-new = [i for i in seq.notes if i.start_time >= 8.0]
-
 def equal(note1,note2):
     if not note1.start_time:
         note1.start_time = 0.0
     dur1, dur2 = note1.end_time-note1.start_time, note2.end_time-note2.start_time
     return dur1 == dur2 and note1.pitch == note2.pitch
 
-print('Cleansing new...')
-for o in origin:
-    for c in new:
-        if equal(o,c):
-            new.remove(c)
+for mag_dir in os.listdir(input_dir):
+    full_dir = osp.join(input_dir,mag_dir)
+    midi_dir = osp.join(full_dir,os.listdir(full_dir)[0])
+    outfile = osp.join(output_dir,os.listdir(full_dir)[0]+'.txt')
 
-print('Writing output file...')
-with open(outfile,'w') as file:
-    for n in origin:
-        start, end, pitch = n.start_time, n.end_time, n.pitch
-        line = ','.join([str(start),str(end),str(pitch)+'\n'])
-        file.write(line)
-    file.write('------------------------\n')
-    for n in new:
-        start, end, pitch = n.start_time, n.end_time, n.pitch
-        line = ','.join([str(start),str(end),str(pitch)+'\n'])
-        file.write(line)
-print('FINISHED')
+    print('Parsing midi files...')
+    seq = note_seq.midi_file_to_note_sequence(midi_dir)
+    origin = [i for i in seq.notes if i.end_time <= 8.0]
+    new = [i for i in seq.notes if i.start_time >= 8.0]
+
+
+    print('Cleansing new...')
+    for o in origin:
+        for c in new:
+            if equal(o,c):
+                new.remove(c)
+
+    print('Writing output file...')
+    with open(outfile,'w') as file:
+        for n in origin:
+            start, end, pitch = n.start_time, n.end_time, n.pitch
+            line = ','.join([str(start),str(end),str(pitch)+'\n'])
+            file.write(line)
+        file.write('------------------------\n')
+        for n in new:
+            start, end, pitch = n.start_time, n.end_time, n.pitch
+            line = ','.join([str(start),str(end),str(pitch)+'\n'])
+            file.write(line)
+    print('FINISHED')
